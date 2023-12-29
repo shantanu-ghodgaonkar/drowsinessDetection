880e1f36c8c31d1304a6d16f8f3d3c2c0592c185
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2023-12-29 17:56:00 -0500

    Basic OpenCV functions tested. New file added to capture image from webcam

--- drowsinessDetectionPy/main.py
+++ drowsinessDetectionPy/main.py
@@ -1,4 +1,25 @@
-# Press the green button in the gutter to run the script.
-# this is in branch1
-if __name__ == '__main__':
-    print('PyCharm')
+# importing libraries
+import cv2
+import numpy as np
+
+image = cv2.imread("pic23-200x141.png")
+
+cv2.imshow('Original Image', image)
+cv2.waitKey(0)
+
+# Gaussian Blur
+Gaussian = cv2.GaussianBlur(image, (7, 7), 0)
+cv2.imshow('Gaussian Blurring', Gaussian)
+cv2.waitKey(0)
+
+# Median Blur
+median = cv2.medianBlur(image, 5)
+cv2.imshow('Median Blurring', median)
+cv2.waitKey(0)
+
+
+# Bilateral Blur
+bilateral = cv2.bilateralFilter(image, 9, 75, 75)
+cv2.imshow('Bilateral Blurring', bilateral)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
